58011eda3916cf5ff2aed1872d8e3b5f62cbc1c1
nawafakbar <nwfakbart@gmail.com>
2025-05-09 23:14:01 +0700

    Layouts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controller\MoviesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [MoviesController::class, 'index']);
